f6edf63e455a51b8bc168cfdc7bae1da1a3e09b3
Iain McManus <iainmcmanus@protonmail.com>
2023-07-07 20:43:05 +1000

    Setup initial version of AI test bed

---
+++ Assets/_PackageRoot/Runtime/CommonCore/Shared/Resources/Scripts/ResourceCommon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources
+{
+    public enum EType
+    {
+        Unknown,
+        
+        Wood,
+        Food,
+        Water
+    }
+}
